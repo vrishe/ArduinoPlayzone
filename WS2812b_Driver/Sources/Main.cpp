9b6d151a50c4cd465f596a904c50d5c764d60075
Vladislav Rishe <vlados.v10.0@gmail.com>
2015-06-20 19:38:14 +0300

    WS2812b driver: - pololu implementation-base WS2812b led strip driver is added.

--- WS2812b_Driver/Sources/Main.cpp
+++ WS2812b_Driver/Sources/Main.cpp
@@ -8,24 +8,46 @@
 #include <Arduino.h>
 #include <stdint.h>
 
+#include "ws2812b.h"
 
-#define BLINK_TIMES(n) ((n) + 1)
+typedef union tagColor {
+	struct {
+		uint8_t r, g, b;
+	};
+	uint8_t value[3];
+} color_rgb;
 
-static uint8_t blinkTimes = 0;
+static int phase;
+static color_rgb colors[60];
+static const color_rgb colors_reference[] = {
+		{ 0xff, 0x00, 0x00 },
+		{ 0x00, 0xff, 0x00 },
+		{ 0x00, 0x00, 0xff }
+};
 
 void loop() {
-	if (++blinkTimes % BLINK_TIMES(4) > 0) {
-		digitalWrite(13, HIGH);
-		delay(196);
-		digitalWrite(13, LOW);
-		delay(128);
-	} else {
-		blinkTimes = 0;
+	int phaseLocal = analogRead(0);
 
-		delay(1024);
+	if (abs(phaseLocal - phase) > 16) {
+		phase = phaseLocal;
+
+		float shift = phase / 1024.f;
+		for (int i = 0; i < _countof(colors); ++i) {
+			const color_rgb color_reference = colors_reference[i % 3];
+
+			colors[i].r += round((color_reference.r - (0xff - color_reference.r)) * shift);
+			colors[i].g += round((color_reference.g - (0xff - color_reference.g)) * shift);
+			colors[i].b	+= round((color_reference.b - (0xff - color_reference.b)) * shift);
+		}
+		ws2812b::write<2, color_rgb>(colors, _countof(colors));
 	}
 }
 
 void setup() {
-	pinMode(13, OUTPUT);
+	DDRB |= B00100000;
+	DDRD |= B00000100;
+
+	ws2812b::write<2, color_rgb>(colors, _countof(colors));
+
+	PORTB = B00100000;
 }
